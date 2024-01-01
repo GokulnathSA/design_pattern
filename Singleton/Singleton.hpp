bf18f87d9229c234f193d484e09a5bd936ea3943
Gokulnath <gokulnath2598@gmail.com>
2024-01-01 13:33:13 +0530

    initial changes

---
+++ Singleton/Singleton.hpp
@@ -0,0 +1,12 @@
+#include <memory>
+#include <mutex>
+
+class Singleton {
+    private:
+        static std::shared_ptr<Singleton> instance;
+        static std::mutex m_singleton;
+        Singleton();
+
+    public:
+        static std::shared_ptr<Singleton> getInstance();
+};
---
+++ Singleton/main.cpp
@@ -0,0 +1,9 @@
+#include "Singleton.hpp"
+
+int main() {
+
+    auto s1 = Singleton::getInstance();
+    auto s2 = Singleton::getInstance();
+
+    return 0;
+}
